8b9c59a33e2722bf4f84217164f5f71f4ef9ff1e
amitsengupta <amitsengupta332@gmail.com>
2026-05-01 22:06:59 +0600

    variable and data types

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var name string;
+	var name string = "Hello";
 
-	name = "World";
+	//  grouped variable
+
+	var (
+		firstName string = "John";
+		lastName string = "Doe";
+	)
+
+	fmt.Println(firstName);
+	fmt.Println(lastName);
 	fmt.Println(name);
 }
